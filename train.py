3ad2f984ba01b21ef4889470ef86d2458a6c7ac6
francisab <francisab@users.noreply.github.com>
2026-04-19 03:08:01 -0700

    exp43: UNEMBEDDING_LR 0.008->0.005

--- train.py
+++ train.py
@@ -813,7 +813,7 @@ WINDOW_PATTERN = "SSSL"   # sliding window pattern: L=full, S=half context
 # Optimization
 TOTAL_BATCH_SIZE = 2 ** 17
 EMBEDDING_LR = 1.0
-UNEMBEDDING_LR = 0.008
+UNEMBEDDING_LR = 0.005
 MATRIX_LR = 0.08
 SCALAR_LR = 1.0
 WEIGHT_DECAY = 0.2
